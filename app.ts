d1468195568f011d03b1b8e7b62c369e42c9cf6f
KBonnicksen <kelseybon@gmail.com>
2019-06-01 23:39:50 -0700

    Switched to VS Code, set up boilerplate functions

--- app.ts
+++ app.ts
@@ -1,28 +1,23 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+﻿/// <reference path="phaser.d.ts"/>
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
     }
+};
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
+var game = new Phaser.Game(config);
 
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
+function preload() {
 }
 
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+function create() {
+}
+
+function update() {
+}
