a6d1812ec3da19aedd3ed26015dc4a31f88a5c32
KBonnicksen <kelseybon@gmail.com>
2019-06-03 13:20:51 -0700

    Added project documentation

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,17 +1,19 @@
 ﻿/// <reference path="phaser.d.ts"/>
 
+//how you configure your game
 var config = {
-    type: Phaser.AUTO,
+    type: Phaser.AUTO, //renderer
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
+    scene: { 
+        preload: preload, //load the assets
         create: create,
         update: update
     }
 };
 
 var game = new Phaser.Game(config);
+
 
 function preload() {
 }
